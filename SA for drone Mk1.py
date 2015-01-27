315834264d91901b471dd71003ec32ab9283e94b
iaminsu <iaminsu@gmail.com>
2015-01-27 10:59:24 -0700

    1차 완성

--- SA for drone Mk1.py
+++ SA for drone Mk1.py
@@ -10,7 +10,7 @@
 # 4. Brown field: part of solution is fixed (warehouses)
 # 5. Point demand representation 
 
-import pysal,  shapefile, networkx, time, cPickle, random
+import pysal,  shapefile, networkx, time, cPickle, random, math
 from shapely.geometry import Point, Polygon, LineString, MultiPoint, MultiPolygon
 from collections import defaultdict
 
@@ -35,14 +35,16 @@ solution_sites = []
 covered_demand = []
 objective_value = 0
 p = 0   #actual p - no. warehouses 
-t0 = 0   #end temperature
-max_iter = 0   #iteration limit
-tf = 0         
-cf = 0.2
+temperature = 0   #end temperature
+max_iter = 5   #iteration limit
+terminate_temp = 0         
+temp_ratio = 0.2
+sa_count = 0
 remove_percent = 0.2 
 fd_fullPayload = 5 
 fd_empty = 10
 fd_delivery = 3.33 
+rc = 0.1
 
 def generateGeometry(in_shp):
     resultingGeometry = []
@@ -80,7 +82,7 @@ def chk_isolation(in_sol):
 
     
 
-def resctricted_cadidates(in_solution):   #for spatial_interchange: 
+def restricted_cadidates(in_solution):   #for spatial_interchange: 
     candis = []
     for site in in_solution:
         candis.extend(F_Fdict[site])
@@ -89,7 +91,7 @@ def resctricted_cadidates(in_solution):   #for spatial_interchange:
     return candis
 
 def spatial_interchange(in_solution):
-    c = resctricted_cadidates(in_solution)
+    c = restricted_cadidates(in_solution)
     flag = True
     while flag == True:
         flag = False
@@ -108,28 +110,62 @@ def spatial_interchange(in_solution):
                         current_obj = [temp_sol, temp_obj]
             if flag == True:
                 in_solution = current_obj[0]
+    return in_solution
                 
-                
+
+def random_fill(in_solution=[]):
+    isolation = True
+    
+    while isolation == True:
+        new_solution = in_solution
+        while len(in_solution) < p:
+            random_pool = restricted_cadidates(new_solution)
+            new_solution.append(random.choice(random_pool))
+        new_isolation = chk_isolation(new_solution)
+        if new_isolation == False:
+            in_solution = new_solution
+            isolation = False
+    return in_solution
             
 
 #initializing seed solution (random) 
-solution_sites.extend(warehouses)
-isol_chk = chk_isolation(solution_sites)
+
+isol_chk = True
 while isol_chk == True:
+    solution_sites = warehouses_ID
     while len(solution_sites) < p:
-        random_pool = resctricted_cadidates(solution_sites)
+        random_pool = restricted_cadidates(solution_sites)
         solution_sites.append(random.choice(random_pool))
     isol_chk = chk_isolation(solution_sites)
 
 
-
-
-while t0 > 0.5:
+while temperature > 0.5:
+    current_solution = solution_sites
+    current_obj = cal_obj(current_solution)
+    new_solution = current_solution
     remove_number = int(remove_percent * p)
     for i in range(remove_number):
-        facils = [x for x in solution_sites if not x in warehouses_ID]
-        solution_sites.remove(random.choice(facils))
+        facils = [x for x in new_solution if not x in warehouses_ID]
+        new_solution.remove(random.choice(facils))
+    new_solution = random_fill(new_solution)
+    new_obj = cal_obj(new_solution)
     
+    if new_obj > current_obj:
+        solution_sites = new_solution
+        sa_count = 0 
+    else:
+        if sa_count > max_iter:
+            sa_count = 0
+            temperature = temperature - (temperature * temp_ratio)
+            if temperature < terminate_temp:
+                break
+        else:
+            sa_count += 1
+            if random.random() < math.exp(((current_obj-new_obj))*rc/temperature):
+                solution_sites = new_solution
+                
+                
+        
         
             
     
